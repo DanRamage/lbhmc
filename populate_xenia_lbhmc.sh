395c8d2d6ab254da66fb780253a82843b9c77a58
DanRamage <ChiefDan@gmail.com>
2017-04-06 14:30:18 -0400

    Init checkin

--- populate_xenia_lbhmc.sh
+++ populate_xenia_lbhmc.sh
@@ -2,6 +2,6 @@
 
 if [ ! -f /tmp/lock_populate_xenia_lbhmc ]; then
   touch /tmp/lock_populate_xenia_lbhmc
-  /home/xeniaprod/scripts/postgresql/feeds/lbhmc/populate_lbhmc.py --ConfigFile=/home/xeniaprod/scripts/postgresql/feeds/lbhmc/lbhmc.ini
+  python /home/xeniaprod/scripts/postgresql/feeds/lbhmc/populate_lbhmc.py --ConfigFile=/home/xeniaprod/scripts/postgresql/feeds/lbhmc/lbhmc.ini
   rm -f /tmp/lock_populate_xenia_lbhmc
 fi
